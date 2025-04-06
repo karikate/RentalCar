4dcc974ca91adb54e1ea80089b2ec9b697ad61db
karikate <k.sarancha96@gmail.com>
2025-04-06 14:07:27 +0300

    add galery cars

--- src/components/CatalogList/CatalogList.jsx
+++ src/components/CatalogList/CatalogList.jsx
@@ -1,13 +1,29 @@
 import { Link } from "react-router-dom";
+import s from "./CatalogList.module.css";
 
 const CatalogList = ({ cars }) => {
   return (
-    <div>
-      <ul>
+    <div className={s.wrapper}>
+      <ul className={s.wrapperList}>
         {cars.map((car) => (
-          <li key={car.id}>
-            <Link to={`/catalog/${car.id}`}>
-              <p>{car.brand}</p>
+          <li key={car.id} className={s.carCard}>
+            <img src={car.img} alt="poster" className={s.image} />
+            <div className={s.titleWrapper}>
+              <h2 className={s.title}>
+                {car.brand}
+                {car.model}
+                {car.year}
+              </h2>
+              <p className={s.price}>{car.rentalPrice}$</p>
+            </div>
+
+            <p>{car.address}</p>
+            <p>{car.rentalCompany}</p>
+            <p>{car.type}</p>
+            <p>{car.mileage}</p>
+
+            <Link to={`/catalog/${car.id}`} className={s.btnReadMore}>
+              Read More
             </Link>
           </li>
         ))}
